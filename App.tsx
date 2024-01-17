684a66b9ca2f5eaaae0bc0c9a19bc3ba9149f9c2
PermatasariRina <105841105920@student.unismuh.ac.id>
2024-01-17 12:03:08 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import Auth from './provider/auth';
+import GuardNavigator from './_layout';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Auth>
+      <GuardNavigator/>
+    </Auth>
   );
 }
 
